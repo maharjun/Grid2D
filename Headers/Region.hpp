6d22e8e0e8c3cbbe70c2914432bc4f0c251e4112
maharjun <arjun210493@gmail.com>
2016-05-15 17:46:59 +0530

    Shifted ColumnRegion and RowRegion to RowColumnRegion.hpp

--- Headers/Region.hpp
+++ Headers/Region.hpp
@@ -5,67 +5,7 @@
 
 #include <MexMemoryInterfacing/Headers/MexMem.hpp>
 #include "Point.hpp"
-#include "RegionExc.hpp"
-#include "LinearRegion.hpp"
-
-class ColumnRegion : public LinearRegion {
-private:
-	uint32_t columnCoord;
-	inline void ValidatePoint(const Point &P) const {
-		// If debug, validate said point
-		if (IS_DEBUG && P.x != this->columnCoord) {
-			throw RegionException::COLUMN_COORD_NOT_MATCHED;
-		}
-	}
-public:
-	ColumnRegion() : columnCoord(UINT32_MAX) {}
-	ColumnRegion(uint32_t regionCoord_, uint32_t regionLenLim_) : LinearRegion(regionLenLim_)
-	{
-		columnCoord = regionCoord_;
-	}
-	inline void insert(const Point &P) {
-		ValidatePoint(P);
-		LinearRegion::insert(P.y);
-	}
-	inline uint32_t getCoord() const
-	{
-		return columnCoord;
-	}
-	inline bool find(const Point& P) const {
-		ValidatePoint(P);
-		return LinearRegion::find(P.y);
-	}
-};
-
-struct RowRegion : LinearRegion {
-private:
-	uint32_t rowCoord;
-	inline void ValidatePoint(const Point &P) const {
-		// If debug, validate said point
-		if (IS_DEBUG && P.y != this->rowCoord) {
-			// ReSharper restore CppRedundantBooleanExpressionArgument
-			throw RegionException::ROW_COORD_NOT_MATCHED;
-		}
-	}
-public:
-	RowRegion() : rowCoord(UINT32_MAX) {}
-	RowRegion(uint32_t regionCoord_, uint32_t regionLenLim_) : LinearRegion(regionLenLim_)
-	{
-		rowCoord = regionCoord_;
-	}
-	inline void insert(const Point &P) {
-		ValidatePoint(P);
-		LinearRegion::insert(P.x);
-	}
-	inline uint32_t getCoord() const
-	{
-		return rowCoord;
-	}
-	inline bool find(const Point &P) const {
-		ValidatePoint(P);
-		return LinearRegion::find(P.x);
-	}
-};
+#include "RowColumnRegion.hpp"
 
 struct Region{
 private:
---
+++ Headers/RowColumnRegion.hpp
@@ -0,0 +1,66 @@
+#ifndef ROW_COLUMN_REGION_HPP
+#define ROW_COLUMN_REGION_HPP
+
+#include "Point.hpp"
+#include "LinearRegion.hpp"
+
+class ColumnRegion : public LinearRegion {
+private:
+	uint32_t columnCoord;
+	inline void ValidatePoint(const Point &P) const {
+		// If debug, validate said point
+		if (IS_DEBUG && P.x != this->columnCoord) {
+			throw RegionException::COLUMN_COORD_NOT_MATCHED;
+		}
+	}
+public:
+	ColumnRegion() : columnCoord(UINT32_MAX) {}
+	ColumnRegion(uint32_t regionCoord_, uint32_t regionLenLim_) : LinearRegion(regionLenLim_)
+	{
+		columnCoord = regionCoord_;
+	}
+	inline void insert(const Point &P) {
+		ValidatePoint(P);
+		LinearRegion::insert(P.y);
+	}
+	inline uint32_t getCoord() const
+	{
+		return columnCoord;
+	}
+	inline bool find(const Point& P) const {
+		ValidatePoint(P);
+		return LinearRegion::find(P.y);
+	}
+};
+
+struct RowRegion : LinearRegion {
+private:
+	uint32_t rowCoord;
+	inline void ValidatePoint(const Point &P) const {
+		// If debug, validate said point
+		if (IS_DEBUG && P.y != this->rowCoord) {
+			// ReSharper restore CppRedundantBooleanExpressionArgument
+			throw RegionException::ROW_COORD_NOT_MATCHED;
+		}
+	}
+public:
+	RowRegion() : rowCoord(UINT32_MAX) {}
+	RowRegion(uint32_t regionCoord_, uint32_t regionLenLim_) : LinearRegion(regionLenLim_)
+	{
+		rowCoord = regionCoord_;
+	}
+	inline void insert(const Point &P) {
+		ValidatePoint(P);
+		LinearRegion::insert(P.x);
+	}
+	inline uint32_t getCoord() const
+	{
+		return rowCoord;
+	}
+	inline bool find(const Point &P) const {
+		ValidatePoint(P);
+		return LinearRegion::find(P.x);
+	}
+};
+
+#endif
