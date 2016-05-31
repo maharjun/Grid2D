4e8f6462acb74eb08e00185d45f56d38544da5cd
Arjun Rao <arjun210493@gmail.com>
2016-05-31 17:17:37 +0530

    BREAKING CHANGES: Renamed to***Point to to***Coords
    Changes:
    
    1.  Renamed to***Point to to***Coords in GridTransform.hpp
    
    2.  Created function toGridPoint which actually returns closest
        grid point.
    
    3.  Added Exception NEgATIVE_GRID_POINT to RegionException to be used
        in toGridPoint()

--- Headers/GridTransform.hpp
+++ Headers/GridTransform.hpp
@@ -2,6 +2,8 @@
 #define TRANSFORM_HPP
 
 #include "Point.hpp"
+#include "RegionExc.hpp"
+#include <MexMemoryInterfacing/Headers/GenericMexIO.hpp>
 #include <cmath>
 #include <type_traits>
 
@@ -12,11 +14,31 @@ struct GridTransform {
     T scaleX, scaleY;
     T shiftX, shiftY;
 
-	inline GenericPoint<T> toActualPoint(const GenericPoint<T> &gridPoint) const {
+	inline GenericPoint<T> toActualCoords(const GenericPoint<T> &gridPoint) const {
 		return GenericPoint<T>(scaleX*gridPoint.x + shiftX, scaleY*gridPoint.y + shiftY);
 	}
-	inline GenericPoint<T> toGridPoint(const GenericPoint<T> &actualPoint) const {
+	inline GenericPoint<T> toGridCoords(const GenericPoint<T> &actualPoint) const {
 		return GenericPoint<T>((actualPoint.x - shiftX)/scaleX, (actualPoint.y - shiftY)/scaleY);
+	}
+	inline Point toGridPoint(const GenericPoint<T> &actualPoint) {
+		// Converts the ActualPoint to Grid Coordinates and rounds off to the
+		// nearest integer and returns
+		// NOTE: This function will fail disastrously if any of the coordinates
+		// of actualPoint are negative. Hence in Debug Mode, a check is introduced for the same
+		auto pointInGridCoords = toGridCoords(actualPoint);
+		if (IS_DEBUG && pointInGridCoords.x < T(-0.5) && pointInGridCoords.y < T(-0.5)) {
+			WriteException(
+				RegionException::NEGATIVE_GRID_POINT,
+				"The Grid Point (%f, %f), which corresponds to the Actual Point (%f, %f), \n"
+					"has one of its components closest to a negative integer and thus cannot be \n"
+					"rounded off to an unsigned integer representing the closest grid point",
+			    pointInGridCoords.x, pointInGridCoords.y,
+			    actualPoint.x, actualPoint.y
+			);
+		}
+		else {
+			return Point(pointInGridCoords.x+T(0.5), pointInGridCoords.y+T(0.5));
+		}
 	}
 	inline bool test() const {
 		std::vector<DoublePoint> TestGridPointVect = {
@@ -29,7 +51,7 @@ struct GridTransform {
 		bool isSuccessful = true;
 		double eps = 1e-10;
 		for(auto &TestGridPoint : TestGridPointVect) {
-			auto ReturnPoint = toGridPoint(toActualPoint(TestGridPoint));
+			auto ReturnPoint = toGridCoords(toActualCoords(TestGridPoint));
 			if (
 				std::abs(ReturnPoint.x - TestGridPoint.x) >= eps ||
 				std::abs(ReturnPoint.y - TestGridPoint.y) >= eps) {
